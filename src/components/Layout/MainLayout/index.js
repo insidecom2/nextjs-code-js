714c4935e3e4d30c27314f3dc467383a56aafa45
insidecom2 <jakkritt26@gmail.com>
2021-07-16 16:38:45 +0700

    fix style menu , index link

--- src/components/Layout/MainLayout/index.js
+++ src/components/Layout/MainLayout/index.js
@@ -20,7 +20,13 @@ import { useRouter } from 'next/router'
 import { useDispatch, useSelector } from 'react-redux'
 import { logout } from 'store/reducers/auth'
 import { setMenu } from 'store/reducers/menu'
-import { HeaderEx, MenuEx, SiderEx, OutLayOut, SpreadLayout } from 'styles/layout/index.style'
+import {
+  HeaderEx,
+  MenuEx,
+  SiderEx,
+  OutLayOut,
+  SpreadLayout
+} from 'styles/layout/index.style'
 const { Header, Content, Sider } = Layout
 const { SubMenu } = Menu
 import AvatarStatus from 'components/Shared/AvatarStatus'
@@ -63,10 +69,11 @@ const MainLayout = (props) => {
     await router.push(`/`)
   }
 
-  return (<SpreadLayout>
-    <div className='Digiboxs'>
-      <HeaderEx style={{ padding: 0 }}>
-      <TitleDashboard>Digiboxs</TitleDashboard>
+  return (
+    <SpreadLayout>
+      <div className="Digiboxs">
+        <HeaderEx style={{ padding: 0 }}>
+          <TitleDashboard>Digiboxs</TitleDashboard>
           <div style={{ float: 'right', margin: '0 10px', cursor: 'pointer' }}>
             <AvatarStatus name={'George Martin'} status={'Admin'} />
             <Dropdown overlay={menu} trigger={['click']}>
@@ -74,125 +81,131 @@ const MainLayout = (props) => {
             </Dropdown>
           </div>
         </HeaderEx>
-    <OutLayOut style={{ minHeight: '100vh' }}>
-      
-      <SiderEx collapsible collapsed={collapsed} onCollapse={onCollapse} >
-        {/* <Divider /> */}
-        <MenuEx
-          selectedKeys={[selectedMenu]}
-          mode="inline"
-          //defaultOpenKeys={['sub1', 'sub2']}
-        >
-          
-          <Menu.Item 
-            key="1"
-            icon={<AppstoreOutlined />}
-            onClick={(e) => onClick(e, 'dashboard')}>
-            แดชบอร์ด
-          </Menu.Item>
-          <div style={{display:"block",margin:"auto",textAlign:"center",borderBottom: "2px solid #DADADA" ,paddingTop:20, width:"80%"}}>
+        <OutLayOut style={{ minHeight: '100vh' }}>
+          <SiderEx collapsible collapsed={collapsed} onCollapse={onCollapse}>
+            {/* <Divider /> */}
+            <MenuEx
+              selectedKeys={[selectedMenu]}
+              mode="inline"
+              //defaultOpenKeys={['sub1', 'sub2']}
+            >
+              <Menu.Item
+                key="1"
+                icon={<AppstoreOutlined />}
+                onClick={(e) => onClick(e, 'dashboard')}>
+                แดชบอร์ด
+              </Menu.Item>
+              <div
+                style={{
+                  display: 'block',
+                  margin: 'auto',
+                  textAlign: 'center',
+                  borderBottom: '2px solid #DADADA',
+                  paddingTop: 20,
+                  width: '80%'
+                }}></div>
+              <Menu.Item
+                key="15"
+                icon={<HomeOutlined />}
+                onClick={(e) => onOpenTab(e, 'http://18.223.136.75')}>
+                หน้าแรก
+              </Menu.Item>
 
-          </div>  
-          <Menu.Item
-            key="15"
-            icon={<HomeOutlined />}
-            onClick={(e) => onOpenTab(e, 'http://3.134.114.182')}>
-            หน้าแรก
-          </Menu.Item>
+              <Menu.Item
+                key="2"
+                icon={<ShoppingOutlined />}
+                onClick={(e) => onClick(e, 'product')}>
+                สินค้า
+              </Menu.Item>
 
-          <Menu.Item
-            key="2"
-            icon={<ShoppingOutlined />}
-            onClick={(e) => onClick(e, 'product')}>
-            สินค้า
-          </Menu.Item>
+              <SubMenu key="sub2" icon={<FileTextOutlined />} title="บทความ">
+                <Menu.Item key="16" onClick={(e) => onClick(e, 'contentType')}>
+                  หมวดหมู่บทความ
+                </Menu.Item>
+                <Menu.Item key="17" onClick={(e) => onClick(e, 'content')}>
+                  บทความ
+                </Menu.Item>
+              </SubMenu>
 
-          <SubMenu key="sub2" icon={<FileTextOutlined />} title="บทความ">
-            <Menu.Item key="16" onClick={(e) => onClick(e, 'contentType')}>
-              หมวดหมู่บทความ
-            </Menu.Item>
-            <Menu.Item key="17" onClick={(e) => onClick(e, 'content')}>
-              บทความ
-            </Menu.Item>
-          </SubMenu>
-
-          {/* <Menu.Item
+              {/* <Menu.Item
             key="3"
             icon={<AntDesignOutlined />}
             onClick={(e) => onClick(e, 'broadcast')}>
             บรอดแคสต์
           </Menu.Item> */}
-          <Menu.Item
-            key="4"
-            icon={<TrophyOutlined />}
-            onClick={(e) => onClick(e, 'promotion')}>
-            จัดการแคมเปญ
-          </Menu.Item>
-          {/* <Menu.Item
+              <Menu.Item
+                key="4"
+                icon={<TrophyOutlined />}
+                onClick={(e) => onClick(e, 'promotion')}>
+                จัดการแคมเปญ
+              </Menu.Item>
+              {/* <Menu.Item
             key="5"
             icon={<BgColorsOutlined />}
             onClick={(e) => onClick(e, 'broadcast')}>
             จัดการธีม
           </Menu.Item> */}
-          <Menu.Item
-            key="6"
-            icon={<HddFilled />}
-            onClick={(e) => onClick(e, 'media')}>
-            คลังจัดเก็บ
-          </Menu.Item>
-          <Menu.Item
-            key="7"
-            icon={<SendOutlined />}
-            onClick={(e) => onClick(e, 'shipping')}>
-            การจัดส่ง
-          </Menu.Item>
-          <SubMenu key="sub1" icon={<ControlFilled />} title="ตั้งค่าสินค้า">
-            <Menu.Item key="8" onClick={(e) => onClick(e, 'category')}>
-              หมวดหมู่
-            </Menu.Item>
-            <Menu.Item key="9" onClick={(e) => onClick(e, 'type')}>
-              ประเภท
-            </Menu.Item>
-            <Menu.Item key="10" onClick={(e) => onClick(e, 'style')}>
-              คุณลักษณะ
-            </Menu.Item>
-            <Menu.Item key="11" onClick={(e) => onClick(e, 'size')}>
-              ขนาด
-            </Menu.Item>
-            <Menu.Item key="12" onClick={(e) => onClick(e, 'material')}>
-              วัสดุ
-            </Menu.Item>
-            <Menu.Item key="13" onClick={(e) => onClick(e, 'printFinish')}>
-              พิมพ์เสร็จสิ้น
-            </Menu.Item>
-            <Menu.Item key="14" onClick={(e) => onClick(e, 'printSides')}>
-              พิมพ์ด้าน
-            </Menu.Item>
-          </SubMenu>
-          <Menu.Item
-            key="100"
-            icon={<SettingOutlined />}
-            onClick={(e) => onClick(e, 'setting')}>
-            ตั้งค่าเว็บไซต์
-          </Menu.Item>
-          <Menu.Item
-            key="101"
-            icon={<PicRightOutlined />}
-            onClick={(e) => onClick(e, 'banner')}>
-            แบนเนอร์
-          </Menu.Item>
-        </MenuEx>
-      </SiderEx>
-      <Layout>
-        
-        <Content style={{ margin: '0 16px', padding: 10 }}>
-          {props.children}
-        </Content>
-      </Layout>
-
-    </OutLayOut>
-    </div>
-  </SpreadLayout>)
+              <Menu.Item
+                key="6"
+                icon={<HddFilled />}
+                onClick={(e) => onClick(e, 'media')}>
+                คลังจัดเก็บ
+              </Menu.Item>
+              <Menu.Item
+                key="7"
+                icon={<SendOutlined />}
+                onClick={(e) => onClick(e, 'shipping')}>
+                การจัดส่ง
+              </Menu.Item>
+              <SubMenu
+                key="sub1"
+                icon={<ControlFilled />}
+                title="ตั้งค่าสินค้า">
+                <Menu.Item key="8" onClick={(e) => onClick(e, 'category')}>
+                  หมวดหมู่
+                </Menu.Item>
+                <Menu.Item key="9" onClick={(e) => onClick(e, 'type')}>
+                  ประเภท
+                </Menu.Item>
+                <Menu.Item key="10" onClick={(e) => onClick(e, 'style')}>
+                  คุณลักษณะ
+                </Menu.Item>
+                <Menu.Item key="11" onClick={(e) => onClick(e, 'size')}>
+                  ขนาด
+                </Menu.Item>
+                <Menu.Item key="12" onClick={(e) => onClick(e, 'material')}>
+                  วัสดุ
+                </Menu.Item>
+                <Menu.Item key="13" onClick={(e) => onClick(e, 'printFinish')}>
+                  พิมพ์เสร็จสิ้น
+                </Menu.Item>
+                <Menu.Item key="14" onClick={(e) => onClick(e, 'printSides')}>
+                  พิมพ์ด้าน
+                </Menu.Item>
+              </SubMenu>
+              <Menu.Item
+                key="100"
+                icon={<SettingOutlined />}
+                onClick={(e) => onClick(e, 'setting')}>
+                ตั้งค่าเว็บไซต์
+              </Menu.Item>
+              <Menu.Item
+                key="101"
+                icon={<PicRightOutlined />}
+                onClick={(e) => onClick(e, 'banner')}>
+                แบนเนอร์
+              </Menu.Item>
+            </MenuEx>
+          </SiderEx>
+          <Layout>
+            <Content style={{ margin: '0 16px', padding: 10 }}>
+              {props.children}
+            </Content>
+          </Layout>
+        </OutLayOut>
+      </div>
+    </SpreadLayout>
+  )
 }
 
 export default MainLayout
--- src/styles/layout/index.style.js
+++ src/styles/layout/index.style.js
@@ -3,24 +3,23 @@ import { Layout, Menu } from 'antd'
 
 const { Header, Sider } = Layout
 
-
 export const SpreadLayout = styled.div`
-background: #82E3E3;
-padding: 60px;
-div.Digiboxs {
-  background-color: #FFFFFF;
-  box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.25);
-  border-radius: 42px !important;
-  padding-bottom: 40px;
-}
+  background: #82e3e3;
+  padding: 60px;
+  div.Digiboxs {
+    background-color: #ffffff;
+    box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.25);
+    border-radius: 42px !important;
+    padding-bottom: 40px;
+  }
 `
 
 export const OutLayOut = styled(Layout)`
-   padding:40px;
-   margin-top: 10px;
-   margin-right:30px;
-   margin-left: 30px;
-   border-radius: 48px;
+  padding: 40px;
+  margin-top: 10px;
+  margin-right: 30px;
+  margin-left: 30px;
+  border-radius: 48px;
 `
 
 export const AvatarTextStatus = styled.label`
@@ -28,7 +27,7 @@ export const AvatarTextStatus = styled.label`
   font-weight: normal;
   font-size: 16px;
   line-height: 20px;
-  color: #6A707E;
+  color: #6a707e;
   padding-right: 10px;
   span:after {
     content: url('https://media.discordapp.net/attachments/775978814693441568/844422554948337684/Group_18.png');
@@ -44,48 +43,49 @@ export const Logo = styled.div`
 
 export const HeaderEx = styled(Header)`
   border-radius: 42px !important;
-  background: #FFFFFF !important;
+  background: #ffffff !important;
   padding-left: 20px !important;
 `
 
 export const SiderEx = styled(Sider)`
-      .ant-menu.ant-menu-root.ant-menu-vertical.ant-menu-light {
-        border-radius: 29px;
-      }
-      .ant-menu-item {
-       display: flex;
-      }
-      .ant-menu-item svg{
-        margin:auto;
-      }
-      .ant-layout-sider-children {
-        height: 685px !important;
-      }
-      border-radius: 29px;
-     .ant-menu.ant-menu-root.ant-menu-inline.ant-menu-light {
-      border-radius: 29px;
-     }
+  .ant-menu.ant-menu-root.ant-menu-vertical.ant-menu-light {
+    border-radius: 29px;
+  }
+  .ant-menu-item {
+    display: flex;
+  }
+  .ant-menu-item svg {
+    margin: auto;
+  }
+  .ant-layout-sider-children {
+    height: 685px !important;
+  }
+  border-radius: 29px;
+  .ant-menu.ant-menu-root.ant-menu-inline.ant-menu-light {
+    border-radius: 29px;
+  }
   .ant-layout-sider-children .ant-menu-submenu-title {
-    font-size: 16px !important;
-    //padding-top: 9px !important;
+    font-size: 15px !important;
+    /* padding-top: 5px !important; */
     height: 40px !important;
     max-height: 100% !important;
-    color: #6A707E !important;
+    color: #6a707e !important;
     border-radius: 34px;
   }
 
   .ant-layout-sider-children .ant-menu-submenu-title:hover {
-    background: linear-gradient(180deg, #438AFE -36.03%, #82E3E3 133.09%);
+    background: linear-gradient(180deg, #438afe -36.03%, #82e3e3 133.09%);
     border-radius: 34px;
     height: 40px !important;
   }
-  .ant-layout-sider-children .ant-menu-submenu-title:hover span,.ant-layout-sider-children .ant-menu-submenu-title:hover i {
+  .ant-layout-sider-children .ant-menu-submenu-title:hover span,
+  .ant-layout-sider-children .ant-menu-submenu-title:hover i {
     color: white;
   }
 
   .ant-layout-sider-children {
     border-radius: 29px !important;
-    background: #FFFFFF !important;
+    background: #ffffff !important;
     box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.25);
   }
 
@@ -98,9 +98,9 @@ export const SiderEx = styled(Sider)`
   }
 
   .ant-layout-sider-trigger {
-    color:  #6A707E !important;
+    color: #6a707e !important;
     bottom: unset;
-   // background-color: #f0eeff !important;
+    // background-color: #f0eeff !important;
   }
 `
 
@@ -108,15 +108,15 @@ export const MenuEx = styled(Menu)`
   .ant-menu-item {
     height: 56px !important;
     max-height: 100% !important;
-    padding-top: 10px !important;
+    /* padding-top: 10px !important; */
     margin: 0 !important;
-    border-radius: 34px!important;
+    border-radius: 34px !important;
     height: 41px !important;
     margin-top: 10px !important;
   }
 
   .ant-menu-item.ant-menu-item-selected {
-     background: linear-gradient(180deg, #438AFE -36.03%, #82E3E3 133.09%);
+    background: linear-gradient(180deg, #438afe -36.03%, #82e3e3 133.09%);
   }
 
   .ant-menu-item span {
@@ -124,7 +124,7 @@ export const MenuEx = styled(Menu)`
     font-weight: normal;
     font-size: 15px;
     line-height: 51px;
-    color: #6A707E;
+    color: #6a707e;
   }
 
   .ant-menu-item svg {
@@ -140,7 +140,6 @@ export const MenuEx = styled(Menu)`
 
   .ant-menu-item.ant-menu-item-selected:hover {
     border-right: unset !important;
-    
   }
 
   .ant-menu-item.ant-menu-item-selected span,
@@ -149,6 +148,10 @@ export const MenuEx = styled(Menu)`
     font-size: 15px !important;
     font-style: normal;
     font-weight: 500;
+  }
+
+  .ant-menu-submenu.ant-menu-submenu-inline {
+    padding-top: 15px;
   }
 
   .ant-menu.ant-menu-dark.ant-menu-inline-collapsed.ant-menu-root.ant-menu-vertical {
@@ -162,36 +165,35 @@ export const MenuEx = styled(Menu)`
   .ant-menu-submenu.ant-menu-submenu-inline ul li {
     color: #7f63f4 !important;
     font-weight: 300 !important;
-    font-size: 16px !important;
+    font-size: 15px !important;
   }
 
   .ant-menu-submenu.ant-menu-submenu-inline ul li:hover {
-    background: linear-gradient(180deg, #438AFE -36.03%, #82E3E3 133.09%);
+    background: linear-gradient(180deg, #438afe -36.03%, #82e3e3 133.09%);
   }
 
   .ant-menu-submenu.ant-menu-submenu-inline .ant-menu-submenu-title {
     margin: 0 !important;
-    
   }
 
   .ant-menu-submenu.ant-menu-submenu-inline.ant-menu-submenu-open
     .ant-menu-submenu-title {
-    background: linear-gradient(180deg, #438AFE -36.03%, #82E3E3 133.09%);
+    background: linear-gradient(180deg, #438afe -36.03%, #82e3e3 133.09%);
   }
 
   .ant-menu-submenu.ant-menu-submenu-inline.ant-menu-submenu-open
-    .ant-menu-submenu-title i {
-      color: white;
-    }
+    .ant-menu-submenu-title
+    i {
+    color: white;
+  }
 
   .ant-menu-item:hover {
-    background: linear-gradient(180deg, #438AFE -36.03%, #82E3E3 133.09%);
+    background: linear-gradient(180deg, #438afe -36.03%, #82e3e3 133.09%);
   }
   .ant-menu-item:hover span {
-   color: white;
+    color: white;
   }
   .ant-menu-item::after {
     border-right: none;
   }
-  
 `
