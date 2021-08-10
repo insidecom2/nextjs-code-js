7250e2badcb306e5eeb0469e7b1607644e7e6451
insidecom2 <jakkritt26@gmail.com>
2021-08-10 10:53:43 +0700

    allow upload

--- src/utils/images.js
+++ src/utils/images.js
@@ -20,9 +20,9 @@ export function beforeUpload(file) {
   if (!isJpgOrPng) {
     message.error('You can only upload JPG/PNG file!')
   }
-  const isLt2M = file.size / 1024 / 1024 < 2
+  const isLt2M = file.size / 1024 / 1024 <= 5
   if (!isLt2M) {
-    message.error('Image must smaller than 2MB!')
+    message.error('Image must smaller than 5MB!')
   }
   return isJpgOrPng && isLt2M
 }
